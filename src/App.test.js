7099fe1c6f4a125aa0b219966f46e99f5f2deaa6
Ken Holston <git.kholston@gmail.com>
2021-10-24 23:42:59 -0400

    remove boilerplate code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders basic div', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const divElement = screen.getByText(/inventory frontend/i);
+  expect(divElement).toBeInTheDocument();
 });
